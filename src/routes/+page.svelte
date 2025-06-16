9ef9a6fd6995102f28da81bae92fe87e479a167f
Sante <lsantet@unsa.edu.pe>
2025-06-16 03:33:26 -0300

    fix: 🐛 deployment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
   import { goto } from '$app/navigation';
+  import { base } from '$app/paths';
   import { onMount } from 'svelte';
 
   onMount(() => {
     // Redirect inmediato a overview
-    goto('/overview', { replaceState: true });
+    goto(base + '/overview', { replaceState: true });
   });
 </script>
 
